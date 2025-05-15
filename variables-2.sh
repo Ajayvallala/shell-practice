947f756bc4151ca4307e00eb0a8b20afff5865c0
ajay <vallala1602@gmail.com>
2025-05-15 17:16:54 +0530

    variable-2

---
+++ variables-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$1:: Hey $2, How are you?"
+echo "$2:: Hello $1, I am fine. How are you doing"
+echo "$1: I am fine too. What's up?"
+echo "$2:: Nohting, just going to Mars now, will you come?"
+echo "$1:: Sorry, you carry on! I will come once you come back"
+
