d203a75d8d39fa309a536976e96a733965327001
ajay <vallala1602@gmail.com>
2025-05-19 15:44:17 +0530

    conditions

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ $1 -le 10 ]
-then
- echo "Given number $1 is less than 10"
-else
- echo "Given number $1 is not lessthan 10"
-fi  
+if [ $1 -eq 10 ]
+then 
+ echo "Please enter number other than 10"
+ if [ $1 -le 10 ]
+ then
+  echo "Given number $1 is less than 10"
+ else
+  echo "Given number $1 is not lessthan 10"
+ fi  
+fi
