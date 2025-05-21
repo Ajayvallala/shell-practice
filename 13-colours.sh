2996ca718411b66c6533ea79db6c31570fb4f9d4
ajay <vallala1602@gmail.com>
2025-05-21 17:46:07 +0530

    colours

--- 13-colours.sh
+++ 13-colours.sh
@@ -9,19 +9,19 @@ USERID=$(id -u) #Getting user id and storing it in USERID variable
 
 if [ $USERID -ne 0 ]
 then
- echo "$Y You are running the script with normal user , please swith to $R root user $W"
+ echo -e "$Y You are running the script with normal user , please swith to $R root user $W"
  exit 1
 else
- echo "$G You are running the script with root user $W"
+ echo -e "$G You are running the script with root user $W"
 fi
 
 VALIDATE()
 {
 if [ $1 -eq 0 ]
 then
- echo "$G $2 installation successfull $W"
+ echo -e "$G $2 installation successfull $W"
 else 
- echo "$R $2 installation was not successfull $W"
+ echo -e "$R $2 installation was not successfull $W"
  exit 1
 fi
 }
@@ -33,7 +33,7 @@ then
  dnf install nginx -y
  VALIDATE &? nginx
 else
- echo "$Y Nginx package was already installed $W"
+ echo -e "$Y Nginx package was already installed $W"
 fi
 
 
@@ -44,7 +44,7 @@ then
  dnf install mysql -y
  VALIDATE &? mysql
 else
- echo "$Y mysql package was already installed $W"
+ echo -e "$Y mysql package was already installed $W"
 fi
 
 dnf list installed python3  #Chacking is package already installed or not?
@@ -54,5 +54,5 @@ then
  dnf install python3 -y
  VALIDATE &? python3
 else
- echo "$Y python3 package was already installed $W"
+ echo -e "$Y python3 package was already installed $W"
 fi
