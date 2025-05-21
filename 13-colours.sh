9f2ff63f098fe15959b220f0074e17a4485e4194
ajay <vallala1602@gmail.com>
2025-05-21 17:41:17 +0530

    colours

---
+++ 13-colours.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+W=\e[0m
+
+USERID=$(id -u) #Getting user id and storing it in USERID variable
+
+if [ $USERID -ne 0 ]
+then
+ echo "$Y You are running the script with normal user , please swith to $R root user $W"
+ exit 1
+else
+ echo "$G You are running the script with root user $W"
+fi
+
+VALIDATE()
+{
+if [ $1 -eq 0 ]
+then
+ echo "$G $2 installation successfull $W"
+else 
+ echo "$R $2 installation was not successfull $W"
+ exit 1
+fi
+}
+
+dnf list installed nginx #Chacking is package already installed or not?
+
+if [ $? -ne 0 ]
+then
+ dnf install nginx -y
+ VALIDATE &? nginx
+else
+ echo "$Y Nginx package was already installed $W"
+fi
+
+
+dnf list installed mysql  #Chacking is package already installed or not?
+
+if [ $? -ne 0 ]
+then
+ dnf install mysql -y
+ VALIDATE &? mysql
+else
+ echo "$Y mysql package was already installed $W"
+fi
+
+dnf list installed python3  #Chacking is package already installed or not?
+
+if [ $? -ne 0 ]
+then
+ dnf install python3 -y
+ VALIDATE &? python3
+else
+ echo "$Y python3 package was already installed $W"
+fi
