f2d500b180340ff7c8e56b3069685d2a054e2e38
ajay <vallala1602@gmail.com>
2025-05-21 17:34:21 +0530

    colours

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo -e "\e[30m Hello word \e[0m"
 echo -e "\e[31m Hello word \e[0m"
-
+echo -e "\e[32m Hello word \e[0m"
+echo -e "\e[33m Hello word \e[0m"
+echo -e "\e[34m Hello word \e[0m"
+echo -e "\e[35m Hello word \e[0m"
+echo -e "\e[36m Hello word \e[0m"
+echo -e "\e[37m Hello word \e[0m"
 echo "No colour"
