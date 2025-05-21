14f1a70d0eaaf401d4686b947b3f34946e2afc25
ajay <vallala1602@gmail.com>
2025-05-21 20:55:29 +0530

    loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -8,7 +8,7 @@ W="\e[0m"
 LOG_FOLDER="/var/log/shellscript"
 LOG_FILE_NAME=$(echo $0 | cut -d "." -f1)
 LOG="$LOG_FOLDER$LOG_FILE_NAME.log"
-PACKAGES=("nginx" "mysql" "python3")
+PACKAGES=("nginx" "mysql" "python3" "httpd")
 
 mkdir -p $LOG_FOLDER
 
