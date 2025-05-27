bafd1d770fb9cab5d7268ebe144bffb9cd9d88fc
ajay <vallala1602@gmail.com>
2025-05-27 18:29:49 +0530

    old-backups

---
+++ 17-set-e.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "hi how are you?"
+
+echooo "iam fine"
+
+echo "how about you?"
---
+++ Deleting-old-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+USER_ID=$(id -u)
+LOG_FOLDER="/var/log/Shell_scrip/"
+LOG_FILE_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER$LOG_FILE_NAME.log"
+Source_dir="/var/log/roboshop.log"
+
+
+if [ $USER_ID -ne 0 ]
+then
+ echo -e $R"Please switch to root user to run the script"$N
+else
+ echo -e $G"You are running the script with root access"$N
+fi
+
+Files_to_delete=$(find $Source_dir -name "*.log" -mtime +15)
+
+while IFS= read -r filepath
+do
+ 
+ echo "$filepath deleted"
+
+ rm -rf $filepath
+
+done <<<$Files_to_delete
+
+echo "Script execution completed successfully"
+ 
+
+
+
+ 
