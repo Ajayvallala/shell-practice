85101be1c6415322e3ffdc674c5079725d935f14
ajay <vallala1602@gmail.com>
2025-05-28 17:07:07 +0530

    backup

--- 20-Backup.sh
+++ 20-Backup.sh
@@ -19,16 +19,14 @@ then
  echo -e $R"Please switch to root user"$N | tee -a $LOG_FILE
  exit 1
 else
- echo -e $G"You are running the script with root user"$N | tee -a $LOG_FILE
+ echo -e "You are running the script with $G root user"$N | tee -a $LOG_FILE
 fi
-USAGE(){
-    echo -e $R"USAGE:$N sudo sh Backup.sh <Source_Dir> <Destination_Dir> <Days>(Optional)" | tee -a $LOG_FILE
-    exit 1
-}
+
 
 if [ $# -lt 2 ]
 then
- USAGE
+ echo -e $R"USAGE:$N sudo sh Backup.sh <Source_Dir> <Destination_Dir> <Days>(Optional)" | tee -a $LOG_FILE
+ exit 1
 fi
 
 if [ ! -d "$SOURCE_DIR" ] || [ ! -d "$DEST_DIR" ]
@@ -42,13 +40,13 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) &>>$LOG_FILE
 if [ ! -z "$FILES" ]
 then
  FILENAMES=$(echo "$FILES" | tr ' ' '\n' | awk -F "/" '{print $NF}') &>>$LOG_FILE
- echo "Files to Zip are "$FILENAMES"" | tee -a $LOG_FILE
+ echo "Files to Zip are :"$FILENAMES"" | tee -a $LOG_FILE
  TIMESTRAMP=$(date +%F-%H-%M-%S)
  ZIP_FILE="$DEST_DIR/backup-$TIMESTRAMP.zip"
  echo "$FILES" | tr ' ' '\n' | zip -@ "$ZIP_FILE" &>>$LOG_FILE
  if [ -f "$ZIP_FILE" ]
  then
-  echo "ZIP file creation successfull" | tee -a $LOG_FILE
+  echo -e $G"ZIP file creation successfull"$N | tee -a $LOG_FILE
   while IFS= read -r filepath
   do
    echo -e $R"Deleting$N $filepath" | tee -a $LOG_FILE
